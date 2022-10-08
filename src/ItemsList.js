1d7190b2be05785ab9cdad7cc56f9398673d3945
Satish Ganesan <ganesansatishm7@gmail.com>
2022-10-08 19:32:52 +0530

    removed anchor tags from code

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -43,12 +43,12 @@ const items=[
   }
 ];
 
-export function ItemsList({ setCartCount, cartCount }) {
+export function ItemsList({ setCartCount, cartCount}) {
 
   return (
-    <section class="py-5">
-      <div class="container px-4 px-lg-5 mt-5">
-        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+    <section className="py-5">
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
         {
           items.map((item)=>(
             <ItemCard item={item} setCartCount={setCartCount} cartCount={cartCount}/>
@@ -67,20 +67,20 @@ function ItemCard({setCartCount,cartCount,item}){
   const [buttonValue, setButtonValue] = useState('Add to Cart');
 
   return(
-    <div class="col mb-5">
-      <div class="card h-100">
-        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-        <div class="card-body p-4">
-            <div class="text-center">
-              <h5 class="fw-bolder">{item.type}</h5>
-              <div class="d-flex justify-content-center small mb-2">
+    <div className="col mb-5">
+      <div className="card h-100">
+        <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <div className="card-body p-4">
+            <div className="text-center">
+              <h5 className="fw-bolder">{item.type}</h5>
+              <div className="d-flex justify-content-center small mb-2">
                 {item.rating}          
               </div>
               {item.price}
             </div>
           </div>
-        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-      <div class="text-center"><button class="btn btn-outline-dark mt-auto" onClick={()=>{
+        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+      <div className="text-center"><button className="btn btn-outline-dark mt-auto" onClick={()=>{
           if(buttonValue==='Add to Cart'){
             setButtonValue('Remove from Cart');
             setCartCount(cartCount+1);
--- src/Navigation.js
+++ src/Navigation.js
@@ -2,31 +2,31 @@
 export function Navigation({ cartCount }) {
   return (
     <div className="container">
-      <nav class="navbar navbar-expand-lg bg-light fixed-top">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-light fixed-top">
+        <div className="container-fluid">
           Start Bootstrap
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 Home
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 About
               </li>
-              <li class="nav-item dropdown dropdown-toggle" role="button" data-bs-toggle="dropdown"  aria-expanded="false">
+              <li className="nav-item dropdown dropdown-toggle" role="button" data-bs-toggle="dropdown"  aria-expanded="false">
                 Shop
-                <ul class="dropdown-menu">
-                  <li class="dropdown-item">All Products</li>
-                  <li><hr class="dropdown-divider"/></li>
-                  <li class="dropdown-item">Popular Item</li>
-                  <li class="dropdown-item">New Arrivals</li>
+                <ul className="dropdown-menu">
+                  <li className="dropdown-item">All Products</li>
+                  <li><hr className="dropdown-divider"/></li>
+                  <li className="dropdown-item">Popular Item</li>
+                  <li className="dropdown-item">New Arrivals</li>
                 </ul>
               </li>
             </ul>
-            <button className="btn border border-dark"><i class="bi bi-cart-fill me-2"></i>Cart<span className="badge bg-dark text-white ms-1 rounded-pill">{cartCount}</span></button>
+            <button className="btn border border-dark"><i className="bi bi-cart-fill me-2"></i>Cart<span className="badge bg-dark text-white ms-1 rounded-pill">{cartCount}</span></button>
           </div>
         </div>
       </nav>
