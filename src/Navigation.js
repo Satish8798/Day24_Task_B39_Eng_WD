81ae15c83c5a7b640162a3edfe962d73d48bb0b6
Satish Ganesan <ganesansatishm7@gmail.com>
2022-10-08 17:57:38 +0530

    sa

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,27 +4,25 @@ export function Navigation({ cartCount }) {
     <div className="container">
       <nav class="navbar navbar-expand-lg bg-light fixed-top">
         <div class="container-fluid">
-          <a class="navbar-brand">Start Bootstrap</a>
+          Start Bootstrap
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page">Home</a>
+                Home
               </li>
               <li class="nav-item">
-                <a class="nav-link">About</a>
+                About
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Shop
-                </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item">All Products</a></li>
+                  <li> All Products</li>
                   <li><hr /></li>
-                  <li><a class="dropdown-item" >Popular Items</a></li>
-                  <li><a class="dropdown-item" >New Arrivals</a></li>
+                  <li>Popular Items</li>
+                  <li>New Arrivals</li>
                 </ul>
               </li>
             </ul>
